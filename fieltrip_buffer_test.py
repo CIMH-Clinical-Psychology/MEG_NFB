b938f3870905e0255d63b672ed98dc9ce93d7d92
mratzel <89906013+mratzel@users.noreply.github.com>
2021-09-23 15:43:55 +0200

    Update fieltrip_buffer_test.py

--- fieltrip_buffer_test.py
+++ fieltrip_buffer_test.py
@@ -1,35 +1,48 @@
 """
-        Accessing FieldTrip buffer with python
+        Accessing FieldTrip buffer with python while running FieldTrip fileproxy with pre-recorded data
+        FieldTrip fileproxy is running in MATLAB
+        Buffer = localhost:1972
 """
 
 import sys
 from fieldtrip import FieldTrip
 
+# Creating and connecting client to Fieldtrip buffer
 ftc = FieldTrip.Client()
 ftc.connect('localhost', 1972)
 
-while True:
+# Read 100 headers and data frames
+for i in range(10):
     
-    h = ftc.getHeader()
+    # Retrieve header of latest chunk in buffer
+    header = ftc.getHeader()
     
-    if h is None:
+    # Check for errors
+    if header is None:
         print('Failed to retrieve header!')
         sys.exit(1)
     
-    print(h)
-    print(h.labels)
+    # Print general header information (number of channels, sample frequency etc.)
+    print(header)
+    # Print labels of every channel
+    print(header.labels)
     
-    if h.nSamples > 0:
+    # Read data from last sample
+    if header.nSamples > 0:
       print('Trying to read last sample...')
-      index = h.nSamples - 1
-      D = ftc.getData([index, index])
-      print(D)
+      index = header.nSamples - 1
+      data = ftc.getData([index, index])
+      
+      # Print data as array of individual sensor signals
+      print(data)
     
-    if h.nEvents > 0:
+    # Read events --> irrelevant for our real-time application?
+    if header.nEvents > 0:
         print('Trying to read (all) events...')
         E = ftc.getEvents()
     
         for e in E:
             print(e)
 
-# ftc.disconnect()
+# Disconnect client from FieldTrip buffer. This crashes the MATLAB instance running the buffer/fileproxy for unknown reasons.
+ftc.disconnect()
